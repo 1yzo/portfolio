0acf3f2a96ca4f9753830261f3027d9973be292f
Osman Ali <okali@uci.edu>
2018-08-01 16:01:51 -0700

    Init commit

---
+++ client/src/store/configureStore.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import notesReducer from '../reducers/notesReducer';
+import selectedNoteReducer from '../reducers/selectedNoteReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default () => {
+    const store = createStore(
+        combineReducers({
+           // Reducers
+        }),
+        composeEnhancers(applyMiddleware(thunk))
+    );
+    
+    return store;
+};
