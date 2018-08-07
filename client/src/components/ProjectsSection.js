33b87a4003dd207da83616a309621c4570ca78df
Osman Ali <okali@uci.edu>
2018-08-06 22:13:48 -0700

    Add github icon to projects

--- client/src/components/ProjectsSection.js
+++ client/src/components/ProjectsSection.js
@@ -3,19 +3,25 @@ import '../styles/projects-section.css';
 
 const ProjectsSection = () => (
     <div className="projects-section">
-        <div className="projects-section__header">
-            Featured Work
+        <div className="projects-section__header">Featured Work</div>
+        <div className="project-container">
+            <img className="project-img" src="/images/kuficlothing.jpg" alt="Kufi Clothing" />
+            <div className="project-overlay">
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div>kuficlothing.com</div>
+                    <i class="fab fa-github"></i>
+                </div>
+                <p>Responsive e-commerce web application.</p>
+            </div>
         </div>
         <div className="project-container">
             <img className="project-img" src="/images/vlf.jpg" alt="VLF" />
             <div className="project-overlay">
-                valuelife.org
-            </div>
-        </div>
-        <div className="project-container">
-            <img className="project-img" src="/images/kuficlothing.jpg" alt="Kufi Clothing" />
-            <div className="project-overlay">
-                kuficlothing.com
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div>valuelife.org</div>
+                    <i class="fab fa-github"></i>
+                </div>
+                <p>Responsive charity website.</p>
             </div>
         </div>
     </div>
