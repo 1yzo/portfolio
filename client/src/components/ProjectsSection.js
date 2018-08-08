40fb544ac5991fbd3864180f8a228871e4cc65c7
Osman Ali <okali@uci.edu>
2018-08-08 02:31:49 -0700

    Make links to github repo functional

--- client/src/components/ProjectsSection.js
+++ client/src/components/ProjectsSection.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import '../styles/projects-section.css';
 
-const ProjectsSection = () => (
-    <div className="projects-section">
-        <div className="projects-section__header">Featured Work</div>
-        <div className="project-container">
-            <img className="project-img" src="/images/kuficlothing.jpg" alt="Kufi Clothing" />
-            <div className="project-overlay">
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <div>kuficlothing.com</div>
-                    <i class="fab fa-github"></i>
-                </div>
-                <p>Responsive e-commerce web application.</p>
+class ProjectsSection extends React.Component {
+    openLink = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        
+        if (e.target.attributes.link) {
+            const link = e.target.attributes.link.value;
+            window.open(link, '_blank');
+        }
+    }
+    
+    render() {
+        return (
+            <div className="projects-section">
+                <div className="projects-section__header">Featured Work</div>
+                <a className="project-container" href="https://kuficlothing.herokuapp.com" target="_blank" rel="noopener noreferrer">
+                    <img className="project-img" src="/images/kuficlothing.jpg" alt="Kufi Clothing" />
+                    <div className="project-overlay" onClick={this.openLink}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <div>kuficlothing.com</div>
+                            <i className="fab fa-github" link="https://github.com/1yzo/kuficlothing" onClick={this.openLink}></i>
+                        </div>
+                        <p>Responsive e-commerce web application.</p>
+                    </div>
+                </a>
+                <a className="project-container" href="https://valuelife.herokuapp.com" target="_blank" rel="noopener noreferrer">
+                    <img className="project-img" src="/images/vlf.jpg" alt="VLF"/>
+                    <div className="project-overlay" onClick={this.openLink}>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <div>valuelife.org</div>
+                            <i className="fab fa-github" link="https://github.com/1yzo/value-life-foundation-donate" onClick={this.openLink} />
+                        </div>
+                        <p>Responsive charity website.</p>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div className="project-container">
-            <img className="project-img" src="/images/vlf.jpg" alt="VLF" />
-            <div className="project-overlay">
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <div>valuelife.org</div>
-                    <i class="fab fa-github"></i>
-                </div>
-                <p>Responsive charity website.</p>
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default ProjectsSection;
