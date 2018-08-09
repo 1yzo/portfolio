7506409001bc906be9ca202a918f59ca5dbb33a3
Osman Ali <okali@uci.edu>
2018-08-09 02:38:15 -0700

    Implement scroll to links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, animateScroll } from 'react-scroll';
 import '../styles/header.css';
 
 class Header extends React.Component {
@@ -21,9 +22,9 @@ class Header extends React.Component {
             <div className={"header" + (this.state.shouldChange ? ' header--scrolled' : '')}>
                 <div className="header__padding" />
                 <div className="header__logo"><i className="material-icons logo">power_settings_new</i>sman</div>
-                <div className="header__link">About</div>
-                <div className="header__link">Projects</div>
-                <div className="header__link">Contact</div>
+                <Link className="header__link" to="about-section" spy smooth duration={700}>About</Link>
+                <Link className="header__link" to="projects-section" spy smooth duration={700}>Projects</Link>
+            <Link className="header__link" to="contact-form" spy smooth duration={700}>Contact</Link>
                 <div className="header__button"><i className="fas fa-envelope"></i></div>
                 <div className="header__padding" />
             </div>
