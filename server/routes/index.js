e1c4a421668409957931df4c8d134e81ac29fb62
Osman Ali <okali@uci.edu>
2018-08-09 02:58:55 -0700

    Make contact form endpoint in backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+const secrets = process.env.NODE_ENV === 'production' ? '' : require('../secrets');
 
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'iyzo.saab@gmail.com',
+        pass: secrets.emailPass || process.env.EMAIL_PASS
+    }
+});
 
+router.post('/contact', (res, req) => {
+    const { name, email, message, budget } = req.body;
+    
+    transporter.sendMail({
+        from: email,
+        to: 'iyzo.saab@gmail.com',
+        subject: `Project proposal from ${name}`,
+        text: message + '\n - from ' + email + '\n Budget: ' + budget
+    }, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(info.message);
+        }
+    });
+});
 
 module.exports = router;
