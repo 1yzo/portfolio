3c2a77c729fd66df887c8d7cd265bfe87ed1bbf3
Osman Ali <okali@uci.edu>
2018-08-09 02:17:07 -0700

    Finish front-end for contact form

--- client/src/components/ContactSection.js
+++ client/src/components/ContactSection.js
@@ -2,10 +2,60 @@ import React from 'react';
 import "../styles/contact-section.css";
 
 class ContactSection extends React.Component {
+    state = {
+        name: '',
+        email: '',
+        budget: '',
+        details: ''
+    };
+    
+    handleChange = (e) => {
+        const { value, id } = e.target;
+
+        const newState = {
+            ...this.state,
+        };
+        newState[id] = value;
+
+        this.setState(() => newState ); 
+    }
+    
     render() {
         return (
             <div className="contact-section">
-                
+                <div className="contact-section__content">
+                    <form className="contact-form">
+                        <label>Name</label>
+                        <input
+                            id="name"
+                            type="name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                        <label>Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                        <label>Project Budget</label>
+                        <input
+                            id="budget"
+                            type="text"
+                            value={this.state.budget}
+                            onChange={this.handleChange}
+                        />
+                        <label>Project Details </label>
+                        <textarea
+                            id="details"
+                            type="text"
+                            value={this.state.details}
+                            onChange={this.handleChange}
+                        />
+                        <button>Send</button>
+                    </form>
+                </div>
             </div>
         );
     }
