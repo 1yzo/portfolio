739c57c1352aaa6fe33ad8cd43a375b55be97d2b
Osman Ali <okali@uci.edu>
2018-08-10 01:15:44 -0700

    Implement live data pulling into Graph component

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,31 +3,45 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
 const secrets = process.env.NODE_ENV === 'production' ? '' : require('../secrets');
+const sgMail = require('@sendgrid/mail');
 
-
-const transporter = nodemailer.createTransport({
-    service: 'hotmail',
-    auth: {
-        user: 'cacti97@hotmail.com',
-        pass: secrets.emailPass || process.env.EMAIL_PASS
-    }
-});
+sgMail.setApiKey(secrets.sendgridKey || process.env.SENDGRID_API_KEY);
 
 router.post('/contact', (req, res) => {
     const { name, email, message, budget } = req.body;
-
-    transporter.sendMail({
-        from: 'cacti97@hotmail.com',
+    const msg = {
         to: 'iyzo.saab@gmail.com',
+        from: 'iyzo.saab@gmail.com',
         subject: `Project proposal from ${name}`,
         text: message + '\n\nFrom: ' + email + '\nBudget: ' + budget
-    }, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            res.json(info.message);
-        }
-    }); 
+    };
+    sgMail.send(msg);
 });
+
+
+// const transporter = nodemailer.createTransport({
+//     service: 'hotmail',
+//     auth: {
+//         user: 'cacti97@hotmail.com',
+//         pass: secrets.emailPass || process.env.EMAIL_PASS
+//     }
+// });
+
+// router.post('/contact', (req, res) => {
+//     const { name, email, message, budget } = req.body;
+
+//     transporter.sendMail({
+//         from: 'cacti97@hotmail.com',
+//         to: 'iyzo.saab@gmail.com',
+//         subject: `Project proposal from ${name}`,
+//         text: message + '\n\nFrom: ' + email + '\nBudget: ' + budget
+//     }, (error, info) => {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             res.json(info.message);
+//         }
+//     }); 
+// });
 
 module.exports = router;
