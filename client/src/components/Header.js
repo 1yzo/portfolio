395c01c6b618cd8575dfd06ed1b72764e2e6415b
Osman Ali <okali@uci.edu>
2018-08-11 14:09:14 -0700

    Add button transitions and github link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,11 +21,13 @@ class Header extends React.Component {
         return (
             <div className={"header" + (this.state.shouldChange ? ' header--scrolled' : '')}>
                 <div className="header__padding" />
-                <div className="header__logo"><i className="material-icons logo">power_settings_new</i>sman</div>
-                <Link className="header__link" to="about-section" spy smooth duration={700}>About</Link>
-                <Link className="header__link" to="projects-section" spy smooth duration={700} offset={-110}>Projects</Link>
-            <Link className="header__link" to="contact-form" spy smooth duration={700}>Contact</Link>
-                <div className="header__button"><i className="fas fa-envelope"></i></div>
+                <Link className="header__logo" to="about-section" spy smooth duration={700}><i className="material-icons logo">power_settings_new</i>sman</Link>
+                <Link className="header__link" activeClass="header__link--active" to="about-section" spy smooth duration={700}>About</Link>
+                <Link className="header__link" activeClass="header__link--active" to="projects-section" spy smooth duration={700} offset={-120}>Projects</Link>
+                <Link className="header__link" activeClass="header__link--active" to="contact-form" spy smooth duration={700} offset={-125}>Contact</Link>
+                <a className="header__button" href="https://github.com/1yzo" target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-github"></i>
+                </a>
                 <div className="header__padding" />
             </div>
         );
