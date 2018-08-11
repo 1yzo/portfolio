940994055e8c00499e7d360b842df40df4bdfa13
Osman Ali <okali@uci.edu>
2018-08-11 00:20:51 -0700

    Emails now working with sendgrid

--- server/routes/index.js
+++ server/routes/index.js
@@ -18,30 +18,4 @@ router.post('/contact', (req, res) => {
     sgMail.send(msg);
 });
 
-
-// const transporter = nodemailer.createTransport({
-//     service: 'hotmail',
-//     auth: {
-//         user: 'cacti97@hotmail.com',
-//         pass: secrets.emailPass || process.env.EMAIL_PASS
-//     }
-// });
-
-// router.post('/contact', (req, res) => {
-//     const { name, email, message, budget } = req.body;
-
-//     transporter.sendMail({
-//         from: 'cacti97@hotmail.com',
-//         to: 'iyzo.saab@gmail.com',
-//         subject: `Project proposal from ${name}`,
-//         text: message + '\n\nFrom: ' + email + '\nBudget: ' + budget
-//     }, (error, info) => {
-//         if (error) {
-//             console.log(error);
-//         } else {
-//             res.json(info.message);
-//         }
-//     }); 
-// });
-
 module.exports = router;
